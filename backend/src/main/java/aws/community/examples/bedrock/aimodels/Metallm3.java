36f29f233c580f9689cebf53b3d6252ea583cb8c
eddy08 <bhardwajharsh08@gmail.com>
2024-10-06 21:10:12 +0530

    Updated max gen length to 4048

--- backend/src/main/java/aws/community/examples/bedrock/aimodels/Metallm3.java
+++ backend/src/main/java/aws/community/examples/bedrock/aimodels/Metallm3.java
@@ -8,27 +8,27 @@ import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelRequest;
 import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelResponse;
 
 public class Metallm3 {
-    public static final String MODEL_ID = "meta.llama3-70b-instruct-v1:0";
+        public static final String MODEL_ID = "meta.llama3-70b-instruct-v1:0";
 
-    public static String invoke(BedrockRuntimeClient client, String prompt, double temperature, int maxTokens) {
-        JSONObject jsonBody = new JSONObject()
-                .put("prompt", prompt)
-                .put("temperature", temperature)
-                .put("max_gen_len", 512)
-                .put("top_p", 0.9);
-        // .put("max_tokens_to_sample", maxTokens);
+        public static String invoke(BedrockRuntimeClient client, String prompt, double temperature, int maxTokens) {
+                JSONObject jsonBody = new JSONObject()
+                                .put("prompt", prompt)
+                                .put("temperature", temperature)
+                                .put("max_gen_len", 4048)
+                                .put("top_p", 0.9);
+                // .put("max_tokens_to_sample", maxTokens);
 
-        InvokeModelRequest request = InvokeModelRequest.builder()
-                .modelId(MODEL_ID)
-                .body(SdkBytes.fromUtf8String(jsonBody.toString()))
-                .build();
+                InvokeModelRequest request = InvokeModelRequest.builder()
+                                .modelId(MODEL_ID)
+                                .body(SdkBytes.fromUtf8String(jsonBody.toString()))
+                                .build();
 
-        InvokeModelResponse response = client.invokeModel(request);
+                InvokeModelResponse response = client.invokeModel(request);
 
-        String completion = new JSONObject(response.body().asUtf8String())
-                .getString("generation");
+                String completion = new JSONObject(response.body().asUtf8String())
+                                .getString("generation");
 
-        return completion;
-    }
+                return completion;
+        }
 
 }
